1d396ae15060e40bc51e44000a29f3889e9e012a
MehmetJank <92443831+MehmetJank@users.noreply.github.com>
2023-07-14 00:03:25 +0300

    Trying bug fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -38,8 +38,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "b2cca2dcfb396f741a243dd44ae7dc60
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "5cca73bb02ad788f94c6a32c521c61bf",
-"/": "5cca73bb02ad788f94c6a32c521c61bf",
+"index.html": "bbb4fba124ba418d7ef30620844702fa",
+"/": "bbb4fba124ba418d7ef30620844702fa",
 "main.dart.js": "634ed9bf18f18d69a24875a9b44eb13d",
 "manifest.json": "e74af8957b5899dc6da961caee768ec9",
 "version.json": "009c9e65172e010890f7f65fde438006"};
