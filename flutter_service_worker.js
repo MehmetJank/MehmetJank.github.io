cf5e4bbb6bc8d4057ab88777c0c89189dbc59b09
MehmetJank <92443831+MehmetJank@users.noreply.github.com>
2023-07-14 13:01:59 +0300

    Bug fix test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -38,8 +38,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "b2cca2dcfb396f741a243dd44ae7dc60
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "7c175015a4125a414baeca121cc7715a",
-"/": "7c175015a4125a414baeca121cc7715a",
+"index.html": "6bc9b97172eeb28e1824c094c4a952e7",
+"/": "6bc9b97172eeb28e1824c094c4a952e7",
 "main.dart.js": "aa895000b57e1db3de004629dd8cbf49",
 "manifest.json": "e74af8957b5899dc6da961caee768ec9",
 "version.json": "009c9e65172e010890f7f65fde438006"};
